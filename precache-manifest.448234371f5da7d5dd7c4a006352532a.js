b749b088672e7c449f1344bcdd6e36f92e020e76
Philip Lemaster <philiplemaster@yahoo.com>
2020-06-27 00:53:46 -0400

    Updates

---
+++ precache-manifest.448234371f5da7d5dd7c4a006352532a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306ef9f3acc63d9c65d55669d3d5a51a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8bb1f037ebe30f3f7bd3",
+    "url": "/Portfolio/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "5d86edf7d47f17417ccb",
+    "url": "/Portfolio/static/css/main.450e897d.chunk.css"
+  },
+  {
+    "revision": "8bb1f037ebe30f3f7bd3",
+    "url": "/Portfolio/static/js/2.2051ea5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.2051ea5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d86edf7d47f17417ccb",
+    "url": "/Portfolio/static/js/main.b077ed37.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "ddccdbf4c0d51441fe41515b04baded3",
+    "url": "/Portfolio/static/media/coinSplash.ddccdbf4.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "f4a92d02bfd883f6d8e33449f5b19538",
+    "url": "/Portfolio/static/media/jumbo.f4a92d02.jpg"
+  },
+  {
+    "revision": "8cc2d3a80c1ebec192c2f29c08333dc6",
+    "url": "/Portfolio/static/media/linkedIn.8cc2d3a8.svg"
+  },
+  {
+    "revision": "4438deca0c606d8bed3cc758fe08547e",
+    "url": "/Portfolio/static/media/splash.4438deca.gif"
+  },
+  {
+    "revision": "9a35c67b9ac12cd7a8817a11aafdfaec",
+    "url": "/Portfolio/static/media/twitter.9a35c67b.svg"
+  }
+]);
