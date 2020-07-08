1fbba29aae62b112f0373ad92efbb66e8e0691a8
Philip Lemaster <philiplemaster@yahoo.com>
2020-07-08 15:18:56 -0400

    updated projects and README

--- src/assets/arrays/ProjectData.js
+++ src/assets/arrays/ProjectData.js
@@ -1,6 +1,15 @@
 const projectData = [
     {
         id: 1,
+        name: 'Chq Mate',
+        imgSrc: 'https://i.ibb.co/MZ1gf3j/project6.png',
+        description: 'Social media app designed to connect gamers with prospective party members and quest partners.',
+        deploySrc: 'https://chq-mate.herokuapp.com/',
+        codeSrc: 'https://github.com/PhilipLemaster/Chq-Mate',
+        responsibilites: 'Full MERN stack, Auth0, Reactstrap'
+    },
+    {
+        id: 2,
         name: 'BotRS',
         imgSrc: 'https://philiplemaster.github.io/Philip-Lemaster/assets/project4.PNG',
         description: 'Automation for repetitive farming tasks in OSRS.',
@@ -8,46 +17,30 @@ const projectData = [
         responsibilites: 'HTML/CSS, Node.js, Materialize.CSS, Robot.js, Express.js, Passport.js'
     },
     {
-        id: 2,
+        id: 3,
         name: 'Retro City',
         imgSrc: 'https://i.ibb.co/7ymMYR9/F7748-ED4-6-C49-4-A25-8595-C80-E797026-E3.jpg',
         description: 'Comprehensive database of Retro games featuring general information and reference images.',
-        deploySrc: 'https://google.com',
+        deploySrc: 'https://philiplemaster.github.io/Retro-City/',
         codeSrc: 'https://github.com/PhilipLemaster/Retro-City',
         responsibilites: 'HTML/CSS, Javascript, Adobe Illustrator, Foundation.CSS, AJAX, Localstorage'
-    },
-    {
-        id: 3,
-        name: 'README.md Generator',
-        imgSrc: 'https://raw.githubusercontent.com/PhilipLemaster/ReadME-Generator/master/assets/READMEgenerator.gif',
-        description: 'Generate formatted and orderly README.md files from node CLI.',
-        codeSrc: 'https://github.com/PhilipLemaster/ReadME-Generator',
-        responsibilites: 'Node.js, Inquirer.js, fs'
-    },
-    {
-        id: 4,
-        name: 'Burger Tracker',
-        imgSrc: 'https://i.ibb.co/G2M9MD7/project4.png',
-        description: 'Server app to track and update a burger list.',
-        codeSrc: 'https://github.com/PhilipLemaster/Node-Express-Handlebars',
-        responsibilites: 'HTML/CSS, Node.js, mySQL, Express.js, Handlebars, Bootstrap'
-    },
-    {
-        id: 5,
-        name: 'Employee Management System',
-        imgSrc: 'https://i.ibb.co/8557wcC/project5.png',
-        description: 'CLI app to manage and update employees, managers, and departments.',
-        codeSrc: 'https://github.com/PhilipLemaster/Employee-Management-System',
-        responsibilites: 'Node.js, mySQL'
-    },
-    {
-        id: 6,
-        name: 'Chq Mate',
-        imgSrc: 'https://i.ibb.co/MZ1gf3j/project6.png',
-        description: 'Social media app designed to connect gamers with prospective party members and quest partners.',
-        codeSrc: 'https://github.com/PhilipLemaster/Chq-Mate',
-        responsibilites: 'Full MERN stack, Auth0, Reactstrap'
     }
+    // {
+    //     id: 3,
+    //     name: 'README.md Generator',
+    //     imgSrc: 'https://raw.githubusercontent.com/PhilipLemaster/ReadME-Generator/master/assets/READMEgenerator.gif',
+    //     description: 'Generate formatted and orderly README.md files from node CLI.',
+    //     codeSrc: 'https://github.com/PhilipLemaster/ReadME-Generator',
+    //     responsibilites: 'Node.js, Inquirer.js, fs'
+    // },
+    // {
+    //     id: 4,
+    //     name: 'Employee Management System',
+    //     imgSrc: 'https://i.ibb.co/8557wcC/project5.png',
+    //     description: 'CLI app to manage and update employees, managers, and departments.',
+    //     codeSrc: 'https://github.com/PhilipLemaster/Employee-Management-System',
+    //     responsibilites: 'Node.js, mySQL'
+    // }
 ]
 
 export default projectData
