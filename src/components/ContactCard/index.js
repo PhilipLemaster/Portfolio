83ea523d01f8d182d50ce8a0e73b97613f638bcb
Philip Lemaster <philiplemaster@yahoo.com>
2020-07-18 02:46:41 -0400

    contact page mobile update

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -6,8 +6,8 @@ function ContactCard() {
         <div className='contactWhole'>
             <h1 className='contactHead'>Contact Me</h1>
             <div className='contactBody'>
-                <h3>(980)355-1739</h3>
-                <h3>philiplemaster@yahoo.com</h3>
+                <h3 id='phone'>(980)355-1739</h3>
+                <h3 id='email'>philiplemaster@yahoo.com</h3>
             </div>
         </div>
     )
